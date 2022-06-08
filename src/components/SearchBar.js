b152764b6f328169d6e9bdae8327f30f4c91403f
Kelly Kaufman <kellykaufman12@gmail.com>
2022-06-08 11:55:44 -0700

    creates function for logging input value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./SearchBar.css";
-import useState from "react";
+import { useState } from "react";
 
 /* Add everything related to searching & filtering the data*/
-const SearchBar = () => {
-  const [data, setData] = useState([]);
 
-  <form action="/" method="get">
-    <label htmlFor="header-search">
-      <div className="searchbar-label">
-        <b> Github Search Feature</b>
-      </div>
-    </label>
-    <input type="text" id="" placeholder="Search github" name="s" />
-    <button type="submit">Search</button>
-  </form>;
+const SearchBar = () => {
+  // const [data, setData] = useState([]);
+
+  const [userInput, setUserInput] = useState("");
+
+  const changeValue = (value) => {
+    setUserInput(value);
+    console.log(userInput);
+  };
+
+  return (
+    <form action="/" method="get">
+      <label htmlFor="header-search">
+        <div className="searchbar-label">
+          <b> Github Search Feature</b>
+        </div>
+      </label>
+      <input
+        onChange={(event) => changeValue(event.target.value)}
+        type="text"
+        id=""
+        placeholder="Search github"
+        name="s"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 
 export default SearchBar;
