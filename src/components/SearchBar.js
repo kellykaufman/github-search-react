ca29eb5cb25ff474750ebe4556d913c6c72072bf
Kelly Kaufman <kellykaufman12@gmail.com>
2022-06-08 11:15:57 -0700

    Adds css to searchbar label and establishes state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "./SearchBar.css";
+import useState from "react";
 
 /* Add everything related to searching & filtering the data*/
-const SearchBar = () => (
+const SearchBar = () => {
+  const [data, setData] = useState([]);
+
   <form action="/" method="get">
     <label htmlFor="header-search">
-      <div style={{ color: "white" }}>
+      <div className="searchbar-label">
         <b> Github Search Feature</b>
       </div>
     </label>
     <input type="text" id="" placeholder="Search github" name="s" />
     <button type="submit">Search</button>
-  </form>
-);
+  </form>;
+};
 
 export default SearchBar;
